ea4c437be915b938822c266360ceb871791cb2c5
olena-kharchenko <harchenko.es1990@gmail.com>
2020-09-21 21:10:40 +0300

    refactor: change createGalleryMarkup

--- js/gallery-create.js
+++ js/gallery-create.js
@@ -1,25 +1,46 @@
 import itemsDefault from './gallery-items.js';
 
-/* <li class="gallery__item">
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+
+const galleryList = document.querySelector('.js-gallery');
+const galleryMarkup = createGalleryMarkup(itemsDefault);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
   <a
     class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+    href="${original}"
   >
     <img
       class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</li>; */
+</li>
+    `;
+    })
+    .join('');
+}
 
-const galleryList = document.querySelector('.js-gallery');
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
-/*
- * Пишем функцию для создания разметки галереи
- */
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-modal"].
+// // Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+// Закрытие модального окна по клику на div.lightbox__overlay.
+// Закрытие модального окна по нажатию клавиши ESC.
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
 
+/* 2 вариант создания разметки createElement
+---------------------------------------------
 const createGalleryMarkup = elements => {
   return elements.map(element => {
     const listEl = document.createElement('li');
@@ -45,6 +66,4 @@ const createGalleryMarkup = elements => {
 
 const itemsOfGallery = createGalleryMarkup(itemsDefault);
 
-galleryList.append(...itemsOfGallery);
-
-console.log(galleryList);
+galleryList.append(...itemsOfGallery);*/
